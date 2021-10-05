81885280540690072a43f4891d7c66eeb3b09064
Martin Bak <bakmartin@gmail.com>
2021-10-05 10:25:45 -0400

    Add claim copy to clipboard boxes

--- idp-wizard/src/app/components/IdentityProviderWizard/AzureWizard/Steps/AzureStepThree.tsx
+++ idp-wizard/src/app/components/IdentityProviderWizard/AzureWizard/Steps/AzureStepThree.tsx
@@ -4,12 +4,96 @@ import {
   StackItem,
   Text,
   TextVariants,
+  Form,
+  Card,
+  CardBody,
+  FormGroup,
+  ClipboardCopy,
+  Flex,
+  FlexItem,
 } from "@patternfly/react-core";
 import React from "react";
 import azureStep7Image from "@app/images/azure/azure-7.png";
 import azureStep8Image from "@app/images/azure/azure-8.png";
+import { ArrowRightIcon } from "@patternfly/react-icons";
+
+interface IClaims {
+  name: string;
+  value: string;
+}
 
 export function AzureStepThree() {
+  const claimNames = [
+    {
+      name: "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress",
+      value: "user.mail",
+    },
+    {
+      name: "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname",
+      value: "user.givenname",
+    },
+    {
+      name: "http://schemas.microsoft.com/identity/claims/name",
+      value: "user.userprincipalname",
+    },
+    {
+      name: "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname",
+      value: "user.surname",
+    },
+  ];
+
+  const renderClipboardCopyHeader = () => {
+    return (
+      <Flex style={{ padding: "5px" }}>
+        <FlexItem
+          style={{ width: "550px", fontSize: "16px", fontWeight: "bold" }}
+        >
+          Claim Name
+        </FlexItem>
+        <FlexItem>
+          <ArrowRightIcon />
+        </FlexItem>
+        <FlexItem
+          style={{ width: "250px", fontSize: "16px", fontWeight: "bold" }}
+        >
+          Value
+        </FlexItem>
+      </Flex>
+    );
+  };
+
+  const renderClipboardCopy = (vals: IClaims) => {
+    console.log(vals.name, vals.value);
+    return (
+      <Flex style={{ padding: "5px" }}>
+        <FlexItem>
+          <ClipboardCopy
+            isReadOnly
+            hoverTip="Copy"
+            clickTip="Copied"
+            className="clipboard-copy"
+            style={{ width: "550px", fontSize: "8px" }}
+          >
+            {vals.name}
+          </ClipboardCopy>
+        </FlexItem>
+        <FlexItem>
+          <ArrowRightIcon />
+        </FlexItem>
+        <FlexItem>
+          <ClipboardCopy
+            isReadOnly
+            hoverTip="Copy"
+            clickTip="Copied"
+            style={{ width: "250px", fontSize: "8px" }}
+          >
+            {vals.value}
+          </ClipboardCopy>
+        </FlexItem>
+      </Flex>
+    );
+  };
+
   return (
     <Stack hasGutter>
       <StackItem>
@@ -22,13 +106,27 @@ export function AzureStepThree() {
         </Text>
       </StackItem>
       <StackItem>
-        <img src={azureStep7Image} alt="Step 2.1" />
+        <img src={azureStep7Image} alt="Step 2.1" className="step-image" />
       </StackItem>
       <StackItem>
         <Text component={TextVariants.h2}>
           <br />
           Fill in the following Attribute Statements and select "Next".
         </Text>
+      </StackItem>
+      <StackItem>
+        <Card style={{ width: "1000px" }}>
+          <CardBody>
+            <Form>
+              <FormGroup fieldId="copy-form">
+                {renderClipboardCopyHeader()}
+                {claimNames.map((claim) => {
+                  return renderClipboardCopy(claim);
+                })}
+              </FormGroup>
+            </Form>
+          </CardBody>
+        </Card>
       </StackItem>
       <StackItem>
         <img src={azureStep8Image} alt="Step 2.1" className="step-image" />
