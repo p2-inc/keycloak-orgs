69368e65c5ffea2db8524b7b6767545b5ca473bb
Jeff Patzer <jpatzer@gmail.com>
2021-12-08 21:33:38 -0700

    configure postcss to use autoprefixer and nested

--- idp-wizard/webpack.common.js
+++ idp-wizard/webpack.common.js
@@ -19,6 +19,7 @@ module.exports = (env) => {
                 experimentalWatchApi: true,
               },
             },
+            "astroturf/loader",
           ],
         },
         {
--- idp-wizard/webpack.dev.js
+++ idp-wizard/webpack.dev.js
@@ -1,11 +1,11 @@
-const path = require('path');
+const path = require("path");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 const { stylePaths } = require("./stylePaths");
 const HOST = process.env.HOST || "localhost";
 const PORT = process.env.PORT || "9000";
 
-module.exports = merge(common('development'), {
+module.exports = merge(common("development"), {
   mode: "development",
   devtool: "eval-source-map",
   devServer: {
@@ -16,17 +16,15 @@ module.exports = merge(common('development'), {
     inline: true,
     historyApiFallback: true,
     overlay: true,
-    open: true
+    open: true,
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        include: [
-          ...stylePaths
-        ],
-        use: ["style-loader", "css-loader"]
-      }
-    ]
-  }
+        include: [...stylePaths],
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+    ],
+  },
 });
