b9fbd760c750a737dd59ca9f8f6760982cb1e90f
Jeff Patzer <jpatzer@gmail.com>
2021-12-12 17:31:55 -0700

    formatting

--- idp-wizard/src/app/hooks/useKeycloakAdminApi.ts
+++ idp-wizard/src/app/hooks/useKeycloakAdminApi.ts
@@ -2,29 +2,28 @@ import KcAdminClient from "@keycloak/keycloak-admin-client";
 import { GrantTypes } from "@keycloak/keycloak-admin-client/lib/utils/auth";
 
 const settings = {
-    baseUrl: process.env.KEYCLOAK_URL,
-    realmName: process.env.REALM,
-    // requestConfig: {
-    //   /* Axios request config options https://github.com/axios/axios#request-config */
-    // },
-  };
+  baseUrl: process.env.KEYCLOAK_URL,
+  realmName: process.env.REALM,
+  // requestConfig: {
+  //   /* Axios request config options https://github.com/axios/axios#request-config */
+  // },
+};
 
-  const credentials = {
-    grantType: "client_credentials" as GrantTypes,
-    clientId: process.env.CLIENT_ID || '',
-    clientSecret: process.env.CLIENT_SECRET || '    ',
-  };
+const credentials = {
+  grantType: "client_credentials" as GrantTypes,
+  clientId: process.env.CLIENT_ID || "",
+  clientSecret: process.env.CLIENT_SECRET || "    ",
+};
 
-  export const useKeycloakAdminApi = () => {
-  
-    const kcAdminClient = new KcAdminClient(settings);
-    //TODO: Do not create a new token for each request. 
-    const setKcAdminClientAccessToken = async () => {
-        await SetAccessToken();
+export const useKeycloakAdminApi = () => {
+  const kcAdminClient = new KcAdminClient(settings);
+  //TODO: Do not create a new token for each request.
+  const setKcAdminClientAccessToken = async () => {
+    await SetAccessToken();
 
-        async function SetAccessToken() {
-          await kcAdminClient.auth(credentials);
-        }
+    async function SetAccessToken() {
+      await kcAdminClient.auth(credentials);
     }
-    return [kcAdminClient, setKcAdminClientAccessToken] as const;
+  };
+  return [kcAdminClient, setKcAdminClientAccessToken] as const;
 };
