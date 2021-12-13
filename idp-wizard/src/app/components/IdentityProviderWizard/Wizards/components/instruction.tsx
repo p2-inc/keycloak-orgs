a5e3e796a0075796535dae50536f0213f13c9622
Jeff Patzer <jpatzer@gmail.com>
2021-12-13 09:01:57 -0700

    update types

--- idp-wizard/src/app/components/IdentityProviderWizard/Wizards/components/instruction.tsx
+++ idp-wizard/src/app/components/IdentityProviderWizard/Wizards/components/instruction.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { ReactElement } from "react";
 import { Flex, FlexItem, Text, TextVariants } from "@patternfly/react-core";
 import cs from "classnames";
 
 export interface InstructionProps {
-  text?: string;
+  text?: string | ReactElement;
   component: JSX.Element;
 }
 export function InstructionComponent({ text, component }: InstructionProps) {
