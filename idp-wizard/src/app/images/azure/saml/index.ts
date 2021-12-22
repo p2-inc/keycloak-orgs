7933a7ef9207aef0611384d34581b5601351113a
Garth <244253+xgp@users.noreply.github.com>
2021-12-22 19:32:42 +0100

    cleaned up image imports for azure saml screenshots

--- idp-wizard/src/app/images/azure/saml/index.ts
+++ idp-wizard/src/app/images/azure/saml/index.ts
@@ -2,6 +2,7 @@ import AzureSaml1 from "./azure-1.png";
 import AzureSaml1a from "./azure-1a.png";
 import AzureSaml2 from "./azure-2.png";
 import AzureSaml3 from "./azure-3.png";
+import AzureSaml3 from "./azure-3a.png";
 import AzureSaml4 from "./azure-4.png";
 import AzureSaml5 from "./azure-5.png";
 import AzureSaml6 from "./azure-6.png";
@@ -11,14 +12,13 @@ import AzureSaml9 from "./azure-9.png";
 import AzureSaml10 from "./azure-10.png";
 import AzureSaml11 from "./azure-11.png";
 import AzureSaml12 from "./azure-12.png";
-import AzureSaml13 from "./azure-13.png";
-import AzureSaml14 from "./azure-14.png";
 
 export {
   AzureSaml1,
   AzureSaml1a,
   AzureSaml2,
   AzureSaml3,
+  AzureSaml3a,
   AzureSaml4,
   AzureSaml5,
   AzureSaml6,
@@ -28,6 +28,4 @@ export {
   AzureSaml10,
   AzureSaml11,
   AzureSaml12,
-  AzureSaml13,
-  AzureSaml14,
 };
