a87bbf2d926593fd1c88748bb144b78f567be599
Garth <244253+xgp@users.noreply.github.com>
2022-01-25 01:20:15 +0100

    read keycloak config from json file. make sure silent-check-sso.html and keycloak.json are served/included by webpack.

--- idp-wizard/src/keycloak.ts
+++ idp-wizard/src/keycloak.ts
@@ -1,12 +1,15 @@
 import Keycloak from 'keycloak-js';
 
 // Setup Keycloak instance as needed
-// Pass initialization options as required or leave blank to load from 'keycloak.json'
+const keycloak = Keycloak('./keycloak.json');
+
+/*
 const keycloak = Keycloak({
   "realm": process.env.REALM || "wizard",
   "url": process.env.KEYCLOAK_URL || "https://app.phasetwo.io/auth/",
   "clientId": process.env.CLIENT_ID || "idp-wizard"
 });
+*/
 
 /*
 keycloak.onTokenExpired = () => {
