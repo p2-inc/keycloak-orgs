99147ed606d1e16d7edb208fb2c76697b50c10f7
Jeff Patzer <jpatzer@gmail.com>
2022-02-02 20:13:30 -0700

    auth0 saml file

--- idp-wizard/src/app/components/IdentityProviderWizard/Wizards/Providers/Auth0/SAML/Steps/3.tsx
+++ idp-wizard/src/app/components/IdentityProviderWizard/Wizards/Providers/Auth0/SAML/Steps/3.tsx
@@ -1,56 +1,15 @@
 import React, { FC, useState } from "react";
 import Auth0Step4Image from "@app/images/auth0/SAML/auth0-4SAML.png";
-import {
-  InstructionProps,
-  Step,
-  StepImage,
-} from "@wizardComponents";
-import {
-  Card,
-  CardBody,
-  CardTitle,
-  FileUploadFieldProps,
-} from "@patternfly/react-core";
-import {
-  API_RETURN,
-} from "@app/configurations/api-status";
-import { MetadataFile } from "./forms";
+import { InstructionProps, Step, StepImage } from "@wizardComponents";
+import { Card, CardBody, CardTitle } from "@patternfly/react-core";
+import { API_RETURN } from "@app/configurations/api-status";
+import { MetadataFile } from "@app/components/IdentityProviderWizard/Wizards/components";
 
 interface Step3Props {
   uploadMetadataFile: (file: File) => Promise<API_RETURN>;
 }
 
 export const Auth0StepThree: FC<Step3Props> = ({ uploadMetadataFile }) => {
-  const [metadataFileValue, setMetadataFileValue] = useState("");
-  const [metadataFileName, setMetadataFileName] = useState("");
-  const [isUploading, setIsUploading] = useState(false);
-  const [uploadValid, setUploadValid] = useState<boolean | null>(null);
-/*
-  const handleFileInputChange: FileUploadFieldProps["onChange"] = async (
-    value,
-    filename,
-    event
-  ) => {
-    setMetadataFileName(filename);
-    setMetadataFileValue(value);
-    setUploadValid(null);
-
-    if (!value) return;
-
-    setIsUploading(true);
-
-    const uploadStatus = await uploadMetadataFile(value);
-
-    if (uploadStatus) {
-      setUploadValid(true);
-    } else {
-      setUploadValid(false);
-    }
-
-    setIsUploading(false);
-  };*/
-
-  
   const handleMetadataFileValidation = async ({
     metadataFile,
   }: {
@@ -59,11 +18,12 @@ export const Auth0StepThree: FC<Step3Props> = ({ uploadMetadataFile }) => {
     const resp = await uploadMetadataFile(metadataFile);
     return resp;
   };
+
   const instructions: InstructionProps[] = [
     {
       text: "In the “Usage” section of the popup, click “Download” next to the “Identity Provider Metadata”.",
       component: <StepImage src={Auth0Step4Image} alt="Step 3.1" />,
-    },,
+    },
     {
       component: (
         <Card isFlat>
@@ -73,12 +33,10 @@ export const Auth0StepThree: FC<Step3Props> = ({ uploadMetadataFile }) => {
               handleFormSubmit={handleMetadataFileValidation}
               formActive={true}
             />
-
           </CardBody>
         </Card>
       ),
     },
-    
   ];
 
   return (
