5adc12f9d593e0af8f000bb84f35ac783bc10b84
Garth <244253+xgp@users.noreply.github.com>
2022-02-08 19:53:53 +0100

    formatting

--- idp-wizard/ext/main/java/io/phasetwo/wizard/WizardResourceProvider.java
+++ idp-wizard/ext/main/java/io/phasetwo/wizard/WizardResourceProvider.java
@@ -1,33 +1,26 @@
 package io.phasetwo.wizard;
 
-import org.keycloak.forms.login.freemarker.FreeMarkerLoginFormsProvider;
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Maps;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Method;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
+import javax.activation.MimetypesFileTypeMap;
 import javax.ws.rs.*;
 import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriBuilder;
-import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.core.PathSegment;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 import lombok.extern.jbosslog.JBossLog;
-import org.keycloak.common.Version;
+import org.keycloak.forms.login.LoginFormsProvider;
+import org.keycloak.forms.login.freemarker.FreeMarkerLoginFormsProvider;
 import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.RealmModel;
 import org.keycloak.services.resource.RealmResourceProvider;
 import org.keycloak.theme.Theme;
-import org.keycloak.forms.login.LoginFormsProvider;
-import org.keycloak.models.RealmModel;
-import java.net.URI;
-import java.net.URLConnection;
-import javax.activation.MimetypesFileTypeMap;
-import java.lang.reflect.Method;
-import java.util.Locale;
 
 @JBossLog
 public class WizardResourceProvider implements RealmResourceProvider {
@@ -43,28 +36,35 @@ public class WizardResourceProvider implements RealmResourceProvider {
     return this;
   }
 
-  /**
-   * awful hack version for keycloak-x
-   */
+  /** awful hack version for keycloak-x */
   @GET
   @Produces(MediaType.TEXT_HTML)
   public Response wizard() {
     String wizardResources = ".";
     Theme theme = getTheme("wizard");
     RealmModel realm = session.getContext().getRealm();
-    LoginFormsProvider form = session.getProvider(LoginFormsProvider.class).setAttribute("wizardResources", wizardResources).setAttribute("realmName", realm.getName());
-    FreeMarkerLoginFormsProvider fm = (FreeMarkerLoginFormsProvider)form;
+    LoginFormsProvider form =
+        session
+            .getProvider(LoginFormsProvider.class)
+            .setAttribute("wizardResources", wizardResources)
+            .setAttribute("realmName", realm.getName());
+    FreeMarkerLoginFormsProvider fm = (FreeMarkerLoginFormsProvider) form;
     try {
-      Method processTemplateMethod = fm.getClass().getDeclaredMethod("processTemplate", Theme.class, String.class, Locale.class);
+      Method processTemplateMethod =
+          fm.getClass()
+              .getDeclaredMethod("processTemplate", Theme.class, String.class, Locale.class);
       processTemplateMethod.setAccessible(true);
-      Response resp = (Response) processTemplateMethod.invoke(fm, theme, "wizard.ftl", session.getContext().resolveLocale(null));
+      Response resp =
+          (Response)
+              processTemplateMethod.invoke(
+                  fm, theme, "wizard.ftl", session.getContext().resolveLocale(null));
       return resp;
     } catch (Exception e) {
       log.warn("Could not call processTemplate on FreeMarkerLoginFormsProvider", e);
     }
     return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
   }
-  
+
   /* this version isn't working in Keycloak-X
   @GET
   @Produces(MediaType.TEXT_HTML)
@@ -75,29 +75,31 @@ public class WizardResourceProvider implements RealmResourceProvider {
     return session.getProvider(LoginFormsProvider.class).setAttribute("wizardResources", wizardResources).setAttribute("realmName", realm.getName()).createForm("wizard.ftl");
   }
   */
-  
+
   @GET
   @Path("{path: ^(200|fonts|images|main|site).*}")
-  public Response staticResources(@PathParam("path") final String path) throws IOException{
+  public Response staticResources(@PathParam("path") final String path) throws IOException {
     String fileName = getLastPathSegment(session.getContext().getUri());
     Theme theme = getTheme("wizard");
     InputStream resource = theme.getResourceAsStream(path);
     String mimeType = getMimeType(fileName);
-    log.infof("%s [%s] (%s)", path, mimeType, null==resource ? "404" : "200");
+    log.infof("%s [%s] (%s)", path, mimeType, null == resource ? "404" : "200");
     return null == resource
         ? Response.status(Response.Status.NOT_FOUND).build()
         : Response.ok(resource, mimeType).build();
   }
-  
+
   @GET
   @Path("/keycloak.json")
   @Produces(MediaType.APPLICATION_JSON)
   public Response keycloakJson() {
     RealmModel realm = session.getContext().getRealm();
     UriInfo uriInfo = session.getContext().getUri();
-    Map json = ImmutableMap.of("realm", realm.getName(),
-                               "auth-server-url", getBaseUrl(uriInfo),
-                               "resource", "idp-wizard");
+    Map json =
+        ImmutableMap.of(
+            "realm", realm.getName(),
+            "auth-server-url", getBaseUrl(uriInfo),
+            "resource", "idp-wizard");
     return Response.ok(json).build();
   }
 
@@ -106,7 +108,7 @@ public class WizardResourceProvider implements RealmResourceProvider {
     if (u != null && u.endsWith("/")) u = u.substring(0, u.length() - 1);
     return u;
   }
-    
+
   /*
   @GET
   @Produces(MediaType.TEXT_HTML)
@@ -131,7 +133,7 @@ public class WizardResourceProvider implements RealmResourceProvider {
     return session.getProvider(LoginFormsProvider.class).setAttribute("wizardResources", wizardResources).setAttribute("realmName", realm.getName()).createForm("wizard.ftl");
   }
   */
-  
+
   private Theme getTheme(String name) {
     try {
       return session.theme().getTheme(name, Theme.Type.LOGIN);
@@ -139,7 +141,7 @@ public class WizardResourceProvider implements RealmResourceProvider {
       throw new WebApplicationException(Response.Status.INTERNAL_SERVER_ERROR);
     }
   }
-  
+
   private String getLastPathSegment(UriInfo uriInfo) {
     List<PathSegment> segs = uriInfo.getPathSegments();
     if (segs != null && segs.size() > 0) {
@@ -148,20 +150,20 @@ public class WizardResourceProvider implements RealmResourceProvider {
     return null;
   }
 
-  private static final String MIME_TYPES = "text/javascript js\n" +
-      "text/css css\n" +
-      "font/woff woff\n" +
-      "font/woff2 woff2\n" +
-      "application/json json webmanifest map\n" +
-      "image/svg+xml svg\n";
+  private static final String MIME_TYPES =
+      "text/javascript js\n"
+          + "text/css css\n"
+          + "font/woff woff\n"
+          + "font/woff2 woff2\n"
+          + "application/json json webmanifest map\n"
+          + "image/svg+xml svg\n";
 
   private String getMimeType(String fileName) {
-    MimetypesFileTypeMap map = new MimetypesFileTypeMap(); 
+    MimetypesFileTypeMap map = new MimetypesFileTypeMap();
     map.addMimeTypes(MIME_TYPES); // loading from mime.types on classpath not working
     return map.getContentType(fileName);
   }
 
   @Override
   public void close() {}
-
 }
