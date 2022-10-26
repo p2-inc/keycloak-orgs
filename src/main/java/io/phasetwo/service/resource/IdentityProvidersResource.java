e788016d93bbe6c4ca90ab922333fb245673b516
Łukasz Dywicki <luke@code-house.org>
2022-10-26 21:13:52 +0200

    Remove ZWSP characters found in source code, align compiler version.
    Signed-off-by: Łukasz Dywicki <luke@code-house.org>

--- src/main/java/io/phasetwo/service/resource/IdentityProvidersResource.java
+++ src/main/java/io/phasetwo/service/resource/IdentityProvidersResource.java
@@ -71,7 +71,7 @@ public class IdentityProvidersResource extends OrganizationAdminResource {
     representation.getConfig().put("syncMode", "FORCE");
     representation.getConfig().put("hideOnLoginPage", "true");
     representation.getConfig().put(ORG_OWNER_CONFIG_KEY, organization.getId());
-    representation.setPostBrokerLoginFlowAlias​(ORG_AUTH_FLOW_ALIAS);
+    representation.setPostBrokerLoginFlowAlias(ORG_AUTH_FLOW_ALIAS);
     //  - firstBrokerLoginFlowAlias
   }
 
