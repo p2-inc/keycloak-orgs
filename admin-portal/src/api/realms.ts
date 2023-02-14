43f6cbd2a52247e376082195e96a862ceaa2405f
Garth <244253+xgp@users.noreply.github.com>
2023-02-14 20:55:31 +0100

    show api approach (#3)
    * show api approach
    
    * additional api methods

---
+++ admin-portal/src/api/realms.ts
@@ -0,0 +1,10 @@
+import { apiConfig, apiRealm } from './helpers';
+import { AttributesApi, EventsApi, EventRepresentation, RealmAttributeRepresentation, WebhookRepresentation } from '@p2-inc/js-sdk';
+
+const attributesApi = new AttributesApi(apiConfig);
+const eventsApi = new EventsApi(apiConfig);                                                          
+
+export const Realms = {
+
+
+}
