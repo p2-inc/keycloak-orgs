74bba3890afe2c6bebe23217ee45eb78332df936
pnzrr <pnzrrr@pm.me>
2023-02-14 21:16:26 -0700

    format api files

--- admin-portal/src/api/realms.ts
+++ admin-portal/src/api/realms.ts
@@ -1,10 +1,13 @@
-import { apiConfig, apiRealm } from './helpers';
-import { AttributesApi, EventsApi, EventRepresentation, RealmAttributeRepresentation, WebhookRepresentation } from '@p2-inc/js-sdk';
+import { apiConfig, apiRealm } from "./helpers";
+import {
+  AttributesApi,
+  EventsApi,
+  EventRepresentation,
+  RealmAttributeRepresentation,
+  WebhookRepresentation,
+} from "@p2-inc/js-sdk";
 
 const attributesApi = new AttributesApi(apiConfig);
-const eventsApi = new EventsApi(apiConfig);                                                          
+const eventsApi = new EventsApi(apiConfig);
 
-export const Realms = {
-
-
-}
+export const Realms = {};
--- admin-portal/src/api/users.ts
+++ admin-portal/src/api/users.ts
@@ -1,52 +1,75 @@
-import { apiConfig, apiRealm } from './helpers';
-import { AccountApi, AccountRepresentation, CredentialRepresentation, ClientRepresentation, DeviceRepresentation, GroupRepresentation, SessionRepresentation, LinkedAccountRepresentation } from '@p2-inc/js-sdk';
+import { apiConfig, apiRealm } from "./helpers";
+import {
+  AccountApi,
+  AccountRepresentation,
+  CredentialRepresentation,
+  ClientRepresentation,
+  DeviceRepresentation,
+  GroupRepresentation,
+  SessionRepresentation,
+  LinkedAccountRepresentation,
+} from "@p2-inc/js-sdk";
 
 const accountApi = new AccountApi(apiConfig);
 
 export const Users = {
+  getAccount: async function (): Promise<AccountRepresentation> {
+    return accountApi.getAccount({
+      realm: apiRealm,
+      userProfileMetadata: true,
+    });
+  },
 
-    getAccount: async function (): Promise<AccountRepresentation> {
-        return accountApi.getAccount({ realm: apiRealm, userProfileMetadata: true });
-    },
+  updateAccount: async function (
+    account: AccountRepresentation
+  ): Promise<void> {
+    return accountApi.updateAccount({
+      realm: apiRealm,
+      accountRepresentation: account,
+    });
+  },
 
-    updateAccount: async function (account: AccountRepresentation): Promise<void> {
-        return accountApi.updateAccount({ realm: apiRealm, accountRepresentation: account });
-    },
+  getCredentials: async function (): Promise<Array<CredentialRepresentation>> {
+    return accountApi.getCredentials({ realm: apiRealm });
+  },
 
-    getCredentials: async function (): Promise<Array<CredentialRepresentation>> {
-        return accountApi.getCredentials({ realm: apiRealm });
-    },
+  deleteCredential: async function (credentialId: string): Promise<void> {
+    return accountApi.deleteCredential({
+      realm: apiRealm,
+      credentialId: credentialId,
+    });
+  },
 
-    deleteCredential: async function (credentialId: string): Promise<void> {
-        return accountApi.deleteCredential({ realm: apiRealm, credentialId: credentialId });
-    },
+  getApplications: async function (): Promise<Array<ClientRepresentation>> {
+    return accountApi.getApplications({ realm: apiRealm });
+  },
 
-    getApplications: async function (): Promise<Array<ClientRepresentation>> {
-        return accountApi.getApplications({ realm: apiRealm });
-    },
+  getDevices: async function (): Promise<Array<DeviceRepresentation>> {
+    return accountApi.getDevices({ realm: apiRealm });
+  },
 
-    getDevices: async function (): Promise<Array<DeviceRepresentation>> {
-        return accountApi.getDevices({ realm: apiRealm });
-    },
+  getSessions: async function (): Promise<Array<SessionRepresentation>> {
+    return accountApi.getSessions({ realm: apiRealm });
+  },
 
-    getSessions: async function (): Promise<Array<SessionRepresentation>> {
-        return accountApi.getSessions({ realm: apiRealm });
-    },
+  deleteCurrentSession: async function (): Promise<void> {
+    return accountApi.deleteCurrentSession({ realm: apiRealm });
+  },
 
-    deleteCurrentSession: async function (): Promise<void> {
-        return accountApi.deleteCurrentSession({ realm: apiRealm });
-    },
-        
-    deleteSession: async function (sessionId: string): Promise<void> {
-        return accountApi.deleteSession({ realm: apiRealm, sessionId: sessionId });
-    },
-    
-    getLinkedAccounts: async function (): Promise<Array<LinkedAccountRepresentation>> {
-        return accountApi.getLinkedAccounts({ realm: apiRealm });
-    },
+  deleteSession: async function (sessionId: string): Promise<void> {
+    return accountApi.deleteSession({ realm: apiRealm, sessionId: sessionId });
+  },
 
-    deleteLinkedAccount: async function (providerId: string): Promise<void> {
-        return accountApi.deleteLinkedProvider({ realm: apiRealm, providerId: providerId });
-    },
+  getLinkedAccounts: async function (): Promise<
+    Array<LinkedAccountRepresentation>
+  > {
+    return accountApi.getLinkedAccounts({ realm: apiRealm });
+  },
 
+  deleteLinkedAccount: async function (providerId: string): Promise<void> {
+    return accountApi.deleteLinkedProvider({
+      realm: apiRealm,
+      providerId: providerId,
+    });
+  },
 };
