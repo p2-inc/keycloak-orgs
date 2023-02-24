99cdbc0d382b1221bd958dff3dd304f70dc9a58e
pnzrr <pnzrrr@pm.me>
2023-02-23 23:37:45 -0700

    pull in data

--- admin-portal/src/pages/organizations/settings/general.tsx
+++ admin-portal/src/pages/organizations/settings/general.tsx
@@ -1,4 +1,13 @@
+import { useParams } from "react-router-dom";
+import { apiRealm } from "store/apis/helpers";
+import { useGetOrganizationByIdQuery } from "store/apis/orgs";
+
 const SettingsGeneral = () => {
+  let { orgId } = useParams();
+  const { data: org } = useGetOrganizationByIdQuery({
+    orgId: orgId!,
+    realm: apiRealm,
+  });
   return <div>General Settings</div>;
 };
 
