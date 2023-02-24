d4c6a7e5426f6b4d6f33d9b22fb28dc50a3b190f
Tomáš Jeřábek <t.jerabek@gmail.com>
2023-02-24 20:01:32 +0100

    feat: update org setting pages

--- admin-portal/src/pages/organizations/settings/general.tsx
+++ admin-portal/src/pages/organizations/settings/general.tsx
@@ -1,3 +1,4 @@
+import SectionHeader from "components/navs/section-header";
 import { useParams } from "react-router-dom";
 import { apiRealm } from "store/apis/helpers";
 import { useGetOrganizationByIdQuery } from "store/apis/orgs";
@@ -8,7 +9,16 @@ const SettingsGeneral = () => {
     orgId: orgId!,
     realm: apiRealm,
   });
-  return <div>General Settings</div>;
+  return (
+    <div className="space-y-4">
+      <div>
+        <SectionHeader
+          title="General Settings"
+          description="One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin."
+        />
+      </div>
+    </div>
+  );
 };
 
 export default SettingsGeneral;
--- admin-portal/src/pages/organizations/settings/sso.tsx
+++ admin-portal/src/pages/organizations/settings/sso.tsx
@@ -1,5 +1,16 @@
+import SectionHeader from "components/navs/section-header";
+
 const SettingsSSO = () => {
-  return <div>SSO Settings</div>;
+  return (
+    <div className="space-y-4">
+      <div>
+        <SectionHeader
+          title="SSO Settings"
+          description="One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin."
+        />
+      </div>
+    </div>
+  );
 };
 
 export default SettingsSSO;
