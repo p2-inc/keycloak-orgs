bfa6d935581477beaa6949101932f4e5982387f3
Tomáš Jeřábek <t.jerabek@gmail.com>
2023-02-27 23:20:01 +0100

    feat: add domain ui

--- admin-portal/src/pages/organizations/domains/add.tsx
+++ admin-portal/src/pages/organizations/domains/add.tsx
@@ -1,3 +1,5 @@
+import Button from "components/elements/forms/buttons/button";
+import FormTextInputWithLabel from "components/elements/forms/inputs/text-input-with-label";
 import RoundedIcon from "components/elements/rounded-icon";
 import { GlobeIcon } from "components/icons";
 import SectionHeader from "components/navs/section-header";
@@ -16,6 +18,14 @@ const DomainsAdd = () => {
         description="One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin."
         icon={addIcon}
       />
+      <div className="py-10 space-y-5">
+        <FormTextInputWithLabel
+          slug="domain-name"
+          label="Domain name"
+          inputArgs={{ placeholder: "Domain name", autoFocus: true }}
+        />
+        <Button isBlackButton={true}>Add domain</Button>
+      </div>
     </div>
   );
 };
