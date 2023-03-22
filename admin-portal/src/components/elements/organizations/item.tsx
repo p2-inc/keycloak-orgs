5bea8259bafa0d8c4d0f5c6c8a1810cbce18cf54
Tomáš Jeřábek <t.jerabek@gmail.com>
2023-03-22 22:11:34 +0100

    feat: add more flags

--- admin-portal/src/components/elements/organizations/item.tsx
+++ admin-portal/src/components/elements/organizations/item.tsx
@@ -2,6 +2,7 @@ import cs from "classnames";
 import { FC } from "react";
 import { Link } from "react-router-dom";
 import { ViewLayoutOptions } from "../forms/switches/view-switch";
+import { config } from "config";
 
 type Props = {
   children: React.ReactNode;
@@ -16,6 +17,43 @@ const SubTitle = ({ children }) => (
   <div className="text-[14px]">{children}</div>
 );
 
+const InnerItem = ({ children, title, subTitle, viewType }) => {
+  return (
+    <div className="relative">
+      <div className="relative z-20">
+        <div
+          className={cs(
+            "col-span-1 flex",
+            "group-hover:border-gray-300 group-hover:bg-white",
+            {
+              "flex-col space-y-5 rounded-md border border-gray-200 bg-gray-50 px-10 py-9":
+                viewType === ViewLayoutOptions.GRID,
+              "flex-row justify-between px-4 py-2":
+                viewType === ViewLayoutOptions.LIST,
+            }
+          )}
+        >
+          <div>
+            <Title>{title}</Title>
+            <SubTitle>{subTitle}</SubTitle>
+          </div>
+          <div className="flex flex-row space-x-8">{children}</div>
+        </div>
+      </div>
+      {viewType === ViewLayoutOptions.GRID && (
+        <div
+          className={cs(
+            "absolute inset-x-3 bottom-0 z-10 h-1/2 rounded-full bg-white opacity-0",
+            "transition-opacity duration-200",
+            "group-hover:opacity-100",
+            "drop-shadow-btn-light group-active:hidden"
+          )}
+        ></div>
+      )}
+    </div>
+  );
+};
+
 const OrganizationItem: FC<Props> = ({
   children,
   link,
@@ -23,50 +61,42 @@ const OrganizationItem: FC<Props> = ({
   subTitle,
   viewType,
 }) => {
+  const featureFlags = config.env.features;
+
   return (
-    <Link
-      to={link}
-      className={cs(
-        "group block",
-        "focus:outline-none focus:ring-1 focus:ring-neutral-50 focus:ring-offset-1",
-        {
-          "md:pb-3": viewType === ViewLayoutOptions.GRID,
-        }
+    <>
+      {featureFlags.orgDetailsEnabled && (
+        <Link
+          to={link}
+          className={cs(
+            "group block",
+            "focus:outline-none focus:ring-1 focus:ring-neutral-50 focus:ring-offset-1",
+            {
+              "md:pb-3": viewType === ViewLayoutOptions.GRID,
+            }
+          )}
+        >
+          <InnerItem title={title} subTitle={subTitle} viewType={viewType}>
+            {children}
+          </InnerItem>
+        </Link>
       )}
-    >
-      <div className="relative">
-        <div className="relative z-20">
-          <div
-            className={cs(
-              "col-span-1 flex",
-              "group-hover:border-gray-300 group-hover:bg-white",
-              {
-                "flex-col space-y-5 rounded-md border border-gray-200 bg-gray-50 px-10 py-9":
-                  viewType === ViewLayoutOptions.GRID,
-                "flex-row justify-between px-4 py-2":
-                  viewType === ViewLayoutOptions.LIST,
-              }
-            )}
-          >
-            <div>
-              <Title>{title}</Title>
-              <SubTitle>{subTitle}</SubTitle>
-            </div>
-            <div className="flex flex-row space-x-8">{children}</div>
-          </div>
+      {!featureFlags.orgDetailsEnabled && (
+        <div
+          className={cs(
+            " block",
+            "focus:outline-none focus:ring-1 focus:ring-neutral-50 focus:ring-offset-1",
+            {
+              "md:pb-3": viewType === ViewLayoutOptions.GRID,
+            }
+          )}
+        >
+          <InnerItem title={title} subTitle={subTitle} viewType={viewType}>
+            {children}
+          </InnerItem>
         </div>
-        {viewType === ViewLayoutOptions.GRID && (
-          <div
-            className={cs(
-              "absolute inset-x-3 bottom-0 z-10 h-1/2 rounded-full bg-white opacity-0",
-              "transition-opacity duration-200",
-              "group-hover:opacity-100",
-              "drop-shadow-btn-light group-active:hidden"
-            )}
-          ></div>
-        )}
-      </div>
-    </Link>
+      )}
+    </>
   );
 };
 
