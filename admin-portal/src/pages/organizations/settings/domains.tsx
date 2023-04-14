18c84e534b447629b29ca767f59ad1d6161b4878
pnzrr <93841792+pnzrr@users.noreply.github.com>
2023-04-14 09:56:54 -0600

    wire up the remove domain functionality (#85)

--- admin-portal/src/pages/organizations/settings/domains.tsx
+++ admin-portal/src/pages/organizations/settings/domains.tsx
@@ -7,19 +7,39 @@ import { PlusIcon } from "components/icons";
 import SectionHeader from "components/navs/section-header";
 import { Link, useParams } from "react-router-dom";
 import { config } from "config";
-import { useGetOrganizationDomainsQuery } from "store/apis/orgs";
+import {
+  OrganizationDomainRepresentation,
+  useGetOrganizationByIdQuery,
+  useGetOrganizationDomainsQuery,
+  useUpdateOrganizationMutation,
+} from "store/apis/orgs";
 import { SettingsProps } from ".";
 import { useTranslation } from "react-i18next";
+import { useState } from "react";
+import ConfirmationModal from "components/elements/confirmation-modal";
+import Alert from "components/elements/alerts/alert";
+import P2Toast from "components/utils/toast";
 
 const { realm } = config.env;
 
 const SettingsDomain = ({ hasManageOrganizationRole }: SettingsProps) => {
   const { t } = useTranslation();
   let { orgId } = useParams();
-  const { data: domains = [], isLoading } = useGetOrganizationDomainsQuery({
+  const [showRemoveConfirmModal, setShowRemoveConfirmModal] =
+    useState<OrganizationDomainRepresentation | null>();
+  const {
+    data: domains = [],
+    isLoading,
+    refetch: refetchDomains,
+  } = useGetOrganizationDomainsQuery({
     realm: realm,
     orgId: orgId!,
   });
+  const { data: org } = useGetOrganizationByIdQuery({
+    realm,
+    orgId: orgId!,
+  });
+  const [updateDomain] = useUpdateOrganizationMutation();
 
   const columns: TableColumns = [
     { key: "domain_name", data: t("domainName") },
@@ -27,28 +47,73 @@ const SettingsDomain = ({ hasManageOrganizationRole }: SettingsProps) => {
     { key: "action", data: "", columnClasses: "flex justify-end" },
   ];
 
-  const rows: TableRows = domains.map((domain) => ({
-    ...domain,
-    verifiedC: domain.verified ? (
-      <div className="text-green-600">{t("verified")}</div>
-    ) : (
-      <div>
-        <span className="mr-2 text-orange-600">{t("verificationPending")}</span>
-      </div>
-    ),
-    action:
-      !hasManageOrganizationRole || domain.verified ? (
-        <></>
+  const removeDomain = async (domain: OrganizationDomainRepresentation) => {
+    const updateOrg = {
+      ...org,
+      domains: org?.domains?.filter((d) => d !== domain.domain_name),
+    };
+    await updateDomain({
+      organizationRepresentation: updateOrg,
+      realm,
+      orgId: orgId!,
+    })
+      .unwrap()
+      .then((r) => {
+        P2Toast({
+          success: true,
+          title: t("removeDomainSuccess", [domain.domain_name]),
+        });
+        refetchDomains();
+      })
+      .catch((e) => {
+        P2Toast({
+          error: true,
+          title: t("removeDomainError", [domain.domain_name, e.data.error]),
+        });
+      })
+      .finally(() => setShowRemoveConfirmModal(null));
+  };
+
+  const rows: TableRows = domains.map((domain) => {
+    let domainActions = <></>;
+    const verifyDomainBtn = (
+      <Link
+        to={`/organizations/${orgId}/domains/verify/${domain.record_value}`}
+      >
+        <Button>{t("verifyDomain")}</Button>
+      </Link>
+    );
+    const removeDomainBtn = (
+      <Button
+        onClick={() => setShowRemoveConfirmModal(domain)}
+        className="ml-2"
+      >
+        {t("removeDomain")}
+      </Button>
+    );
+    if (hasManageOrganizationRole) {
+      domainActions = (
+        <>
+          {!domain.verified && verifyDomainBtn}
+          {removeDomainBtn}
+        </>
+      );
+    }
+
+    return {
+      ...domain,
+      verifiedC: domain.verified ? (
+        <div className="text-green-600">{t("verified")}</div>
       ) : (
         <div>
-          <Link
-            to={`/organizations/${orgId}/domains/verify/${domain.record_value}`}
-          >
-            <Button>{t("verifyDomain")}</Button>
-          </Link>
+          <span className="mr-2 text-orange-600">
+            {t("verificationPending")}
+          </span>
         </div>
       ),
-  }));
+      action: domainActions,
+    };
+  });
 
   return (
     <div className="space-y-4">
@@ -74,6 +139,23 @@ const SettingsDomain = ({ hasManageOrganizationRole }: SettingsProps) => {
       <div>
         <Table columns={columns} rows={rows} isLoading={isLoading} />
       </div>
+      {showRemoveConfirmModal && (
+        <ConfirmationModal
+          open={!!showRemoveConfirmModal}
+          close={() => {
+            setShowRemoveConfirmModal(null);
+          }}
+          modalTitle={t("removeDomainTitle", [
+            showRemoveConfirmModal.domain_name,
+          ])}
+          modalMessage={t("removeDomainQuestion")}
+          onContinue={() =>
+            removeDomain(
+              showRemoveConfirmModal as OrganizationDomainRepresentation
+            )
+          }
+        />
+      )}
     </div>
   );
 };
