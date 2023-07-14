76098633b1fc3d2fd2e60dd884c7837c35c26626
Austin Oyugi <austinoyugi@gmail.com>
2023-07-14 07:06:05 +0300

    added lower expression in name criteria

--- src/main/java/io/phasetwo/service/model/jpa/JpaOrganizationProvider.java
+++ src/main/java/io/phasetwo/service/model/jpa/JpaOrganizationProvider.java
@@ -234,22 +234,19 @@ public class JpaOrganizationProvider implements OrganizationProvider {
         continue;
       }
 
-      switch (key) {
-        case "name":
-          predicates.add(
-              builder.or(
-                  builder.like(root.get("name"), "%" + value.toLowerCase() + "%"),
-                  builder.like(root.get("displayName"), "%" + value.toLowerCase() + "%")));
-          break;
-        default:
-          Join<OrganizationEntity, OrganizationAttributeEntity> attributesJoin =
-              root.join("attributes", JoinType.LEFT);
+      if (key.equals("name")) {
+        predicates.add(
+                builder.or(
+                        builder.like(builder.lower(root.get("name")), "%" + value.toLowerCase() + "%"),
+                        builder.like(builder.lower(root.get("displayName")), "%" + value.toLowerCase() + "%")));
+      } else {
+        Join<OrganizationEntity, OrganizationAttributeEntity> attributesJoin =
+                root.join("attributes", JoinType.LEFT);
 
-          attributePredicates.add(
-              builder.and(
-                  builder.equal(builder.lower(attributesJoin.get("name")), key.toLowerCase()),
-                  builder.equal(builder.lower(attributesJoin.get("value")), value.toLowerCase())));
-          break;
+        attributePredicates.add(
+                builder.and(
+                        builder.equal(builder.lower(attributesJoin.get("name")), key.toLowerCase()),
+                        builder.equal(builder.lower(attributesJoin.get("value")), value.toLowerCase())));
       }
     }
 
