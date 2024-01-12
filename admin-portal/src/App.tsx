2e8fb49d80bb435696003a00b8474016fe20e01a
pnzrr <pnzrrr@pm.me>
2024-01-11 22:25:37 -0700

    move the initial load into the App level to avoid any issues with specific routes

--- admin-portal/src/App.tsx
+++ admin-portal/src/App.tsx
@@ -1,7 +1,29 @@
 import Layout from "components/layouts/layout";
+import { setLanguage } from "i18n";
 import { Outlet } from "react-router-dom";
+import { useGetAccountQuery } from "store/apis/profile";
+import { config } from "config";
+import { useEffect } from "react";
+
+const { supportedLocales, realm } = config.env;
 
 function App() {
+  const hasLocale = (locale: string): boolean => locale in supportedLocales;
+
+  const { data: account } = useGetAccountQuery({
+    userProfileMetadata: true,
+    realm,
+  });
+  useEffect(() => {
+    // load language (after login)
+    if (
+      account?.attributes?.locale &&
+      hasLocale(account?.attributes?.locale[0])
+    ) {
+      setLanguage(account?.attributes?.locale[0]);
+    }
+  }, [account]);
+
   return (
     <Layout>
       <Outlet />
