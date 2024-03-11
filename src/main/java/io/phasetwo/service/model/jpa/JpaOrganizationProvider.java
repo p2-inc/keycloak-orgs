9c99400b910750b29b15500d001ab258df00a4e6
Richard Gonzalez <richard.gonzalez@fastly.com>
2024-03-11 14:48:53 -0400

    removes lower

--- src/main/java/io/phasetwo/service/model/jpa/JpaOrganizationProvider.java
+++ src/main/java/io/phasetwo/service/model/jpa/JpaOrganizationProvider.java
@@ -312,8 +312,9 @@ public class JpaOrganizationProvider implements OrganizationProvider {
 =======
           attributePredicates.add(
               builder.and(
-                builder.equal(builder.lower(attributesJoin.get("name")), key.toLowerCase()),
-                builder.equal(builder.lower(attributesJoin.get("value")), value.toLowerCase())));
+                // this change is intentional (see: https://github.com/fastly/keycloak-orgs/pull/6)
+                builder.equal(attributesJoin.get("name"), key),
+                builder.equal(attributesJoin.get("value"), value)));
           break;
 >>>>>>> ecff613 (match upstream)
       }
