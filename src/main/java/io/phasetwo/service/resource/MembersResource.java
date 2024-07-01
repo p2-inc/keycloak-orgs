c11bdbe38708b8c7e265796d31a729d3130e4983
Mathieu Poussin <mathieu.poussin@philips.com>
2024-07-01 12:03:30 +0200

    Update org members api endpoint to allow to list service accounts

--- src/main/java/io/phasetwo/service/resource/MembersResource.java
+++ src/main/java/io/phasetwo/service/resource/MembersResource.java
@@ -36,12 +36,14 @@ public class MembersResource extends OrganizationAdminResource {
   public Stream<UserRepresentation> getMembers(
       @QueryParam("search") String searchQuery,
       @QueryParam("first") Integer firstResult,
-      @QueryParam("max") Integer maxResults) {
+      @QueryParam("max") Integer maxResults,
+      @QueryParam("includeServiceAccounts") boolean includeServiceAccounts) {
     log.debugf("Get members for %s %s [%s]", realm.getName(), organization.getId(), searchQuery);
     firstResult = firstResult != null ? firstResult : 0;
     maxResults = maxResults != null ? maxResults : Constants.DEFAULT_MAX_RESULTS;
     return organization
         .searchForMembersStream(searchQuery, firstResult, maxResults)
+        .filter(u -> includeServiceAccounts || (u != null && u.getServiceAccountClientLink() == null))
         .map(m -> toRepresentation(session, realm, m));
   }
 
