e6e38a2d45cead448923f3d6f921735933873a43
Max Lay <kalda341@gmail.com>
2024-07-26 09:58:04 +1200

    Remove DomainExtractor email verification check as it is performed elsewhere

--- src/main/java/io/phasetwo/service/auth/idp/DomainExtractor.java
+++ src/main/java/io/phasetwo/service/auth/idp/DomainExtractor.java
@@ -9,7 +9,6 @@ import java.util.Optional;
 final class DomainExtractor {
 
     private static final Logger LOG = Logger.getLogger(DomainExtractor.class);
-    private static final String EMAIL_ATTRIBUTE = "email";
 
     private final HomeIdpDiscoveryConfig config;
 
@@ -25,10 +24,6 @@ final class DomainExtractor {
         String userAttribute = user.getFirstAttribute(config.userAttribute());
         if (userAttribute == null) {
             LOG.warnf("Could not find user attribute '%s' for user '%s'", config.userAttribute(), user.getId());
-            return Optional.empty();
-        }
-        if (EMAIL_ATTRIBUTE.equalsIgnoreCase(config.userAttribute()) && !user.isEmailVerified()) {
-            LOG.warnf("Email address of user '%s' is not verified", user.getId());
             return Optional.empty();
         }
         return extractFrom(userAttribute);
