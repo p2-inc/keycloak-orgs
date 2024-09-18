d88609b86c3d7707c4cdb2fd519d9c2962bf6861
Natalie Ayling <natalie.ayling@fastly.com>
2024-09-18 15:53:29 -0400

    clean up syntax

--- src/main/java/io/phasetwo/service/auth/idp/HomeIdpDiscoverer.java
+++ src/main/java/io/phasetwo/service/auth/idp/HomeIdpDiscoverer.java
@@ -128,15 +128,19 @@ final class HomeIdpDiscoverer {
                     return hasCID && hasForceSSO;
                 })
                 .sorted((o1, o2) -> {
-                    String corp_id = o.getFirstAttribute("corp_id");
-                    String customer_id = o.getFirstAttribute("customer_id");
-                    if(clientID.equals("sigsci-dashboard") && corp_id != null && !corp_id.isEmpty()) {
-                        return -1;
+                    if(clientID.equals("sigsci-dashboard")) {
+                        String corp_id = o1.getFirstAttribute("corp_id");
+                        if(corp_id != null && !corp_id.isEmpty()) {
+                            return -1;
+                        }
                     }
-                    if(!clientID.equals("sigsci-dashboard") && customer_id != null && !customer_id.isEmpty()) {
-                        return -1;
+                    else {
+                        String customer_id = o1.getFirstAttribute("customer_id");
+                        if(customer_id != null && !customer_id.isEmpty()) {
+                            return -1;
+                        }
                     }
-                    else return 1;
+                    return 1;
                 })
                 .sorted((o1, o2) -> {
                     if(o1.getFirstAttribute("customer_id") == userDefaultCID) return -1;
