2b5dfaba49d6b71fdb0d660f0390c89a238657df
rtufisi <rtufisi@phasetwo.io>
2025-09-06 19:55:08 +0300

    Improve identity provider search

--- src/main/java/io/phasetwo/service/util/IdentityProviders.java
+++ src/main/java/io/phasetwo/service/util/IdentityProviders.java
@@ -4,12 +4,16 @@ import static io.phasetwo.service.Orgs.ORG_OWNER_CONFIG_KEY;
 import static io.phasetwo.service.Orgs.ORG_SHARED_IDP_KEY;
 
 import io.phasetwo.service.model.OrganizationModel;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+
+import java.util.*;
+
+import org.keycloak.broker.provider.IdentityProvider;
+import org.keycloak.broker.provider.IdentityProviderFactory;
+import org.keycloak.broker.social.SocialIdentityProvider;
 import org.keycloak.models.Constants;
 import org.keycloak.models.IdentityProviderModel;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.jpa.entities.IdentityProviderEntity;
 import org.keycloak.representations.idm.IdentityProviderRepresentation;
 
 public final class IdentityProviders {
@@ -62,6 +66,46 @@ public final class IdentityProviders {
       representation.getConfig().put(ORG_SHARED_IDP_KEY, "true");
     } else {
       representation.getConfig().put(ORG_SHARED_IDP_KEY, "false");
+    }
+  }
+
+  /* code copied from keycloak */
+  public static IdentityProviderModel toModel(IdentityProviderEntity entity, KeycloakSession session) {
+    if (entity == null) {
+      return null;
+    } else {
+      IdentityProviderModel identityProviderModel = getModelFromProviderFactory(entity.getProviderId(), session);
+      identityProviderModel.setProviderId(entity.getProviderId());
+      identityProviderModel.setAlias(entity.getAlias());
+      identityProviderModel.setDisplayName(entity.getDisplayName());
+      identityProviderModel.setInternalId(entity.getInternalId());
+      Map<String, String> config = new HashMap(entity.getConfig());
+      identityProviderModel.setConfig(config);
+      identityProviderModel.setEnabled(entity.isEnabled());
+      identityProviderModel.setLinkOnly(entity.isLinkOnly());
+      identityProviderModel.setHideOnLogin(entity.isHideOnLogin());
+      identityProviderModel.setTrustEmail(entity.isTrustEmail());
+      identityProviderModel.setAuthenticateByDefault(entity.isAuthenticateByDefault());
+      identityProviderModel.setFirstBrokerLoginFlowId(entity.getFirstBrokerLoginFlowId());
+      identityProviderModel.setPostBrokerLoginFlowId(entity.getPostBrokerLoginFlowId());
+      identityProviderModel.setOrganizationId(entity.getOrganizationId());
+      identityProviderModel.setStoreToken(entity.isStoreToken());
+      identityProviderModel.setAddReadTokenRoleOnCreate(entity.isAddReadTokenRoleOnCreate());
+      return identityProviderModel;
+    }
+  }
+
+  /* code copied from keycloak */
+  private static IdentityProviderModel getModelFromProviderFactory(String providerId, KeycloakSession session) {
+    IdentityProviderFactory factory = (IdentityProviderFactory)session.getKeycloakSessionFactory().getProviderFactory(IdentityProvider.class, providerId);
+    if (factory == null) {
+      factory = (IdentityProviderFactory) session.getKeycloakSessionFactory().getProviderFactory(SocialIdentityProvider.class, providerId);
+    }
+
+    if (factory != null) {
+      return factory.createConfig();
+    } else {
+      return new IdentityProviderModel();
     }
   }
 }
