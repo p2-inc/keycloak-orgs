8df3e2b4be031c118f91b9ffbca432dde26a7c04
Vilmos Nagy <vilmos.nagy@outlook.com>
2026-01-27 12:55:41 +0100

    Add test for when the organization has multiple IDPs each with a different domain

--- src/test/e2e/cypress/fixtures/users.ts
+++ src/test/e2e/cypress/fixtures/users.ts
@@ -12,5 +12,6 @@ const user3: User = { username: "user-3", password: "user-3" };
 
 const idpUser: User = { username: "test@phasetwo.io", password: "test123", firstName: "Ben", lastName: "Big", email: "test@phasetwo.io" };
 const unverifiedIdpUser: User = { username: "unverified@phasetwo.io", password: "test123", firstName: "Ben", lastName: "Big", email: "unverified@phasetwo.io" };
+const authItIdpUser: User = { username: "test@auth.it", password: "test123", firstName: "Ben", lastName: "Big", email: "test@auth.it" };
 
-export { User, user1, user2, user3, idpUser, unverifiedIdpUser }
+export { User, user1, user2, user3, idpUser, unverifiedIdpUser, authItIdpUser }
--- src/test/java/io/phasetwo/web/CypressHomeIdpOrganizationTest.java
+++ src/test/java/io/phasetwo/web/CypressHomeIdpOrganizationTest.java
@@ -7,16 +7,17 @@ import io.github.wimdeblauwe.testcontainers.cypress.CypressContainer;
 import io.github.wimdeblauwe.testcontainers.cypress.CypressTestResults;
 import io.phasetwo.client.openapi.model.OrganizationRepresentation;
 import io.phasetwo.service.representation.LinkIdp;
+import io.phasetwo.service.util.IdentityProviders;
 import jakarta.ws.rs.core.Response;
 import lombok.extern.jbosslog.JBossLog;
 import org.hamcrest.CoreMatchers;
 import org.hamcrest.Matchers;
 import org.jetbrains.annotations.NotNull;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.DynamicContainer;
-import org.junit.jupiter.api.TestFactory;
+import org.jetbrains.annotations.Nullable;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 import org.keycloak.admin.client.resource.RealmResource;
+import org.keycloak.representations.idm.IdentityProviderRepresentation;
 import org.keycloak.representations.idm.RealmRepresentation;
 import org.testcontainers.Testcontainers;
 import org.testcontainers.containers.BindMode;
@@ -30,6 +31,7 @@ import java.util.stream.Stream;
 
 import static io.phasetwo.service.Helpers.*;
 import static io.phasetwo.service.Orgs.ORG_CONFIG_MULTIPLE_IDPS_KEY;
+import static io.phasetwo.service.Orgs.ORG_DOMAIN_CONFIG_KEY;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -41,6 +43,21 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 class CypressHomeIdpOrganizationTest extends AbstractCypressOrganizationTest {
 
     private static final String OIDC_IDP = "oidc-idp";
+    private List<String> knownRealms;
+
+    @BeforeEach
+    public void setup() {
+        knownRealms = new ArrayList<>();
+    }
+
+    @AfterEach
+    public void cleanupKeycloakInstance() {
+        List.copyOf(knownRealms)
+                .forEach(realmName -> {
+                    findRealmByName(realmName).remove();
+                    knownRealms.remove(realmName);
+                });
+    }
 
     @TestFactory
     @DisplayName("Base testcases")
@@ -65,9 +82,7 @@ class CypressHomeIdpOrganizationTest extends AbstractCypressOrganizationTest {
     @DisplayName("Testcases when the bypassLoginPage is true")
     public List<DynamicContainer> bypassLoginEnabledTests() throws IOException, InterruptedException, TimeoutException {
         setupTestKeycloakInstance(false);
-        final var realm = keycloak
-                .realms()
-                .realm("test-realm");
+        final var realm = findRealmByName("test-realm");
 
         final var homeIdpDiscoveryExecution = realm
                 .flows()
@@ -83,7 +98,6 @@ class CypressHomeIdpOrganizationTest extends AbstractCypressOrganizationTest {
                 "",
                 "cypress/e2e/home-idp-organization/bypass-login-enabled.cy.ts"
         );
-        cleanupKeycloakInstance();
         return dynamicContainers;
     }
 
@@ -105,7 +119,8 @@ class CypressHomeIdpOrganizationTest extends AbstractCypressOrganizationTest {
 
         return Stream.of(ForwardToFirstMatch.values()).map(forwardToFirstMatch -> {
             try {
-                setupTestKeycloakInstance(false);
+                setupKeycloakInstanceWithMultipleIdpsEnabledAtOrganization();
+
                 if (forwardToFirstMatch.equals(ForwardToFirstMatch.FALSE)) {
                     modifyAuthenticatorConfig(
                             "test-realm",
@@ -114,21 +129,6 @@ class CypressHomeIdpOrganizationTest extends AbstractCypressOrganizationTest {
                             Map.entry("forwardToFirstMatch", "false")
                     );
                 }
-
-                final var realm = keycloak
-                        .realms()
-                        .realm("test-realm");
-
-                RealmRepresentation realmRepresentation = realm.toRepresentation();
-                Map<String, String> attributes = new HashMap<>(realmRepresentation.getAttributesOrEmpty());
-                realmRepresentation.setAttributes(attributes);
-                attributes.put(ORG_CONFIG_MULTIPLE_IDPS_KEY, "true");
-                realm.update(realmRepresentation);
-
-                createIdentityProviderIn(realm, "second-oidc", false);
-                final var organization = findOrganizationRepresentationByName(realm.toRepresentation(), "org-home");
-                linkIdentityProviderToOrganization(realm.toRepresentation(), organization, "second-oidc");
-
                 List<DynamicContainer> dynamicContainers = runCypressTests(
                         forwardToFirstMatch.testPrefix,
                         "cypress/e2e/home-idp-organization/" + forwardToFirstMatch.testFile
@@ -142,13 +142,85 @@ class CypressHomeIdpOrganizationTest extends AbstractCypressOrganizationTest {
     }
 
     @TestFactory
+    @DisplayName("When the organizaton has mutliple domains and multiple IDPs set up with each IDP linked to different domain that should work correctly")
+    public List<DynamicContainer> testIdpsLinkedToOrgWhenDomainsAreSetForIdps() throws IOException, InterruptedException, TimeoutException {
+        setupKeycloakInstanceWithMultipleIdpsEnabledAtOrganization();
+        final var realm = findRealmByName("test-realm");
+        final var organization = findOrganizationRepresentationByName(realm.toRepresentation(), "org-home");
+        organization.setDomains(List.of("phasetwo.io", "auth.it"));
+        updateOrganizationByRepresentation(organization);
+
+        final var linkToIdp01 = realm.identityProviders().get("oidc-idp").toRepresentation();
+        final var linkToIdp02 = realm.identityProviders().get("second-oidc").toRepresentation();
+
+        IdentityProviders.setAttributeMultivalued(
+                linkToIdp01.getConfig(),
+                ORG_DOMAIN_CONFIG_KEY,
+                Set.of("phasetwo.io")
+        );
+
+        IdentityProviders.setAttributeMultivalued(
+                linkToIdp02.getConfig(),
+                ORG_DOMAIN_CONFIG_KEY,
+                Set.of("auth.it")
+        );
+
+        realm.identityProviders().get(linkToIdp01.getAlias()).update(linkToIdp01);
+        realm.identityProviders().get(linkToIdp02.getAlias()).update(linkToIdp02);
+
+        return runCypressTests(
+                "Organization's Linked IDP has domain set for each IDP",
+                "cypress/e2e/home-idp-organization/multiple-idps-enabled-with-domains-per-idp.cy.ts"
+        );
+    }
+
+    private void setupKeycloakInstanceWithMultipleIdpsEnabledAtOrganization() throws JsonProcessingException {
+        setupTestKeycloakInstance(false);
+        final var secondClientRealm = importRealm("/realms/external-idp.json", "second-external-idp");
+        final var realm = findRealmByName("test-realm");
+
+        RealmRepresentation realmRepresentation = realm.toRepresentation();
+        Map<String, String> attributes = new HashMap<>(realmRepresentation.getAttributesOrEmpty());
+        realmRepresentation.setAttributes(attributes);
+        attributes.put(ORG_CONFIG_MULTIPLE_IDPS_KEY, "true");
+        realm.update(realmRepresentation);
+
+        final var secondIdentityProvider = createIdentityProviderIn(realm, "second-oidc", false, "second-external-idp");
+        updateRedirectUriInClientRealm(secondClientRealm, secondIdentityProvider);
+        final var organization = findOrganizationRepresentationByName(realm.toRepresentation(), "org-home");
+        linkIdentityProviderToOrganization(realm.toRepresentation(), organization, "second-oidc");
+    }
+
+    private RealmRepresentation importRealm(String jsonRepresentationPath) {
+        return importRealm(jsonRepresentationPath, null);
+    }
+
+    private RealmRepresentation importRealm(String jsonRepresentationPath, @Nullable String realmOverride) {
+        RealmRepresentation realm =
+                loadJson(getClass().getResourceAsStream(jsonRepresentationPath),
+                        RealmRepresentation.class);
+        if (realmOverride != null) {
+            realm.setRealm(realmOverride);
+        }
+        importRealm(realm, keycloak);
+        knownRealms.add(realm.getRealm());
+        log.info("realm imported successfully:" + realm.getRealm());
+        return realm;
+    }
+
+    private static RealmResource findRealmByName(String realm) {
+        return keycloak
+                .realms()
+                .realm(realm);
+    }
+
+    @TestFactory
     @DisplayName("The UsernameNoteAuthenticator should ask the username, handle extra steps, and continue with HomeIDP Authentication")
     public List<DynamicContainer> testUsernameInAuthNoteFormWithHomeIdpIfTheresExtraAuthenticationFormPartsBetween() throws IOException, InterruptedException, TimeoutException {
         setupTestKeycloakInstance(false, "/realms/kc-realm-with-home-idp-with-username-auth-note-flow.json");
         final var cypressResult = runCypressTests(
                 "",
                 "cypress/e2e/home-idp-organization/auth-note-form-with-home-idp.cy.ts");
-        cleanupKeycloakInstance();
         return cypressResult;
     }
 
@@ -159,14 +231,11 @@ class CypressHomeIdpOrganizationTest extends AbstractCypressOrganizationTest {
         final var cypressResult = runCypressTests(
                 "",
                 "cypress/e2e/home-idp-organization/home-idp-require-verified-email-test.cy.ts");
-        cleanupKeycloakInstance();
         return cypressResult;
     }
 
     private void modifyAuthenticatorConfig(String realmName, String flowAlias, String providerId, Map.Entry<String, String> modifiedConfig) {
-        final var realm = keycloak
-                .realms()
-                .realm(realmName);
+        final var realm = findRealmByName(realmName);
 
         final var homeIdpDiscoveryExecution = realm
                 .flows()
@@ -188,25 +257,14 @@ class CypressHomeIdpOrganizationTest extends AbstractCypressOrganizationTest {
         Testcontainers.exposeHostPorts(container.getHttpPort());
 
         // import testRealm
-        RealmRepresentation testRealm =
-                loadJson(getClass().getResourceAsStream(testRealmRepresentationLocation),
-                        RealmRepresentation.class);
-        importRealm(testRealm, keycloak);
+        RealmRepresentation testRealm = importRealm(testRealmRepresentationLocation);
 
         // import client realm
-        RealmRepresentation clientRealm =
-                loadJson(getClass().getResourceAsStream("/realms/external-idp.json"),
-                        RealmRepresentation.class);
-        importRealm(clientRealm, keycloak);
-
-        //update redirectUris
-        var redirectUri = "http://host.testcontainers.internal:%s/auth/realms/test-realm/broker/oidc-idp/endpoint".formatted(container.getHttpPort());
-        var clientRep = keycloak.realm(clientRealm.getRealm()).clients().findByClientId("test-realm-client").get(0);
-        clientRep.setRedirectUris(List.of(redirectUri));
-        keycloak.realm(clientRealm.getRealm()).clients().get(clientRep.getId()).update(clientRep);
+        RealmRepresentation clientRealm = importRealm("/realms/external-idp.json");
 
         //create idp
-        createIdentityProviderIn(keycloak.realm("test-realm"), OIDC_IDP, isIdpLinkOnlyValue);
+        final var idp = createIdentityProviderIn(keycloak.realm("test-realm"), OIDC_IDP, isIdpLinkOnlyValue, "external-idp");
+        updateRedirectUriInClientRealm(clientRealm, idp);
 
         //create organization with domain
         var representation = new OrganizationRepresentation().name("org-home").domains(List.of("phasetwo.io"));
@@ -231,6 +289,14 @@ class CypressHomeIdpOrganizationTest extends AbstractCypressOrganizationTest {
 
         // link it
         linkIdentityProviderToOrganization(testRealm, orgRep, OIDC_IDP);
+    }
+
+    private static void updateRedirectUriInClientRealm(RealmRepresentation clientRealm, IdentityProviderRepresentation identityProvider) {
+        //update redirectUris
+        var redirectUri = "http://host.testcontainers.internal:%s/auth/realms/test-realm/broker/%s/endpoint".formatted(container.getHttpPort(), identityProvider.getAlias());
+        var clientRep = keycloak.realm(clientRealm.getRealm()).clients().findByClientId("test-realm-client").get(0);
+        clientRep.setRedirectUris(List.of(redirectUri));
+        keycloak.realm(clientRealm.getRealm()).clients().get(clientRep.getId()).update(clientRep);
     }
 
     private @NotNull OrganizationRepresentation findOrganizationRepresentationById(RealmRepresentation testRealm, String id) throws JsonProcessingException {
@@ -279,6 +345,24 @@ class CypressHomeIdpOrganizationTest extends AbstractCypressOrganizationTest {
         return orgRep;
     }
 
+    private void updateOrganizationByRepresentation(OrganizationRepresentation organizationRepresentation) throws JsonProcessingException {
+        var response2 =
+                given()
+                        .baseUri(container.getAuthServerUrl())
+                        .basePath("realms/" + organizationRepresentation.getRealm() + "/orgs/" + organizationRepresentation.getId())
+                        .contentType("application/json")
+                        .auth()
+                        .oauth2(keycloak.tokenManager().getAccessTokenString())
+                        .and()
+                        .body(toJsonString(organizationRepresentation))
+                        .when()
+                        .put()
+                        .then()
+                        .extract()
+                        .response();
+        assertThat(response2.getStatusCode(), is(Response.Status.NO_CONTENT.getStatusCode()));
+    }
+
     private void linkIdentityProviderToOrganization(
             RealmRepresentation testRealm,
             OrganizationRepresentation orgRep,
@@ -303,9 +387,8 @@ class CypressHomeIdpOrganizationTest extends AbstractCypressOrganizationTest {
         assertThat(response2.getStatusCode(), is(Response.Status.CREATED.getStatusCode()));
     }
 
-    private void createIdentityProviderIn(RealmResource realm, String providerAlias, Boolean isIdpLinkOnlyValue) {
-        org.keycloak.representations.idm.IdentityProviderRepresentation idp =
-                new org.keycloak.representations.idm.IdentityProviderRepresentation();
+    private IdentityProviderRepresentation createIdentityProviderIn(RealmResource realm, String providerAlias, Boolean isIdpLinkOnlyValue, String externalIdpId) {
+        IdentityProviderRepresentation idp = new IdentityProviderRepresentation();
         idp.setAlias(providerAlias);
         idp.setProviderId("oidc");
         idp.setEnabled(true);
@@ -315,7 +398,7 @@ class CypressHomeIdpOrganizationTest extends AbstractCypressOrganizationTest {
                 new ImmutableMap.Builder<String, String>()
                         .put("useJwksUrl", "true")
                         .put("syncMode", "FORCE")
-                        .put("authorizationUrl", "http://host.testcontainers.internal:%s/auth/realms/external-idp/protocol/openid-connect/auth".formatted(container.getHttpPort()))
+                        .put("authorizationUrl", "http://host.testcontainers.internal:%s/auth/realms/%s/protocol/openid-connect/auth".formatted(container.getHttpPort(), externalIdpId))
                         .put("hideOnLoginPage", "")
                         .put("loginHint", "")
                         .put("uiLocales", "")
@@ -324,17 +407,14 @@ class CypressHomeIdpOrganizationTest extends AbstractCypressOrganizationTest {
                         .put("acceptsPromptNoneForwardFromClient", "")
                         .put("validateSignature", "")
                         .put("pkceEnabled", "")
-                        .put("tokenUrl", "http://host.testcontainers.internal:%s/auth/realms/external-idp/protocol/openid-connect/token".formatted(container.getHttpPort()))
+                        .put("tokenUrl", "http://host.testcontainers.internal:%s/auth/realms/%s/protocol/openid-connect/token".formatted(container.getHttpPort(), externalIdpId))
                         .put("clientAuthMethod", "client_secret_post")
                         .put("clientId", "test-realm-client")
                         .put("clientSecret", "secret-123")
                         .build());
-        realm.identityProviders().create(idp);
-    }
-
-    private void cleanupKeycloakInstance() {
-        keycloak.realms().realm("test-realm").remove();
-        keycloak.realms().realm("external-idp").remove();
+        Response response = realm.identityProviders().create(idp);
+        assertThat(response.getStatus(), is(Response.Status.CREATED.getStatusCode()));
+        return realm.identityProviders().get(providerAlias).toRepresentation();
     }
 
     private @NotNull List<DynamicContainer> runCypressTests(String testContainerNamePrefix, String cypressTestFile) throws InterruptedException, TimeoutException, IOException {
