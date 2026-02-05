c55106a3befa47da9f264b453bcbeba6917c51a3
Vilmos Nagy <vnagy@phasetwo.io>
2026-02-05 16:09:36 +0100

    Ignore empty entries in org domain list
    Closes #403

--- src/main/java/io/phasetwo/service/importexport/representation/OrganizationAttributes.java
+++ src/main/java/io/phasetwo/service/importexport/representation/OrganizationAttributes.java
@@ -2,11 +2,15 @@ package io.phasetwo.service.importexport.representation;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSetter;
+import com.fasterxml.jackson.annotation.Nulls;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import io.phasetwo.service.util.EmptyStringAsNullDeserializer;
 import lombok.Data;
 
 @Data
@@ -26,6 +30,8 @@ public class OrganizationAttributes {
   private String url;
 
   @JsonProperty("domains")
+  @JsonDeserialize(contentUsing = EmptyStringAsNullDeserializer.class)
+  @JsonSetter(contentNulls = Nulls.SKIP)
   private Set<String> domains = Sets.newHashSet();
 
   @JsonProperty("attributes")
